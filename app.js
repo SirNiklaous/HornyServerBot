1668727a45f5b1fa98ffdd4df9ec46f0f7040016
SirNiklaous <63741951+SirNiklaous@users.noreply.github.com>
2021-02-27 18:50:11 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const config = require('./config.json');
+const Client = require('./src/Client.js');
+const { Intents } = require('discord.js');
+
+global.__basedir = __dirname;
+
+// Client setup
+const intents = new Intents();
+intents.add(
+  'GUILD_PRESENCES',
+  'GUILD_MEMBERS',
+  'GUILDS',
+  'GUILD_VOICE_STATES',
+  'GUILD_MESSAGES',
+  'GUILD_MESSAGE_REACTIONS'
+);
+const client = new Client(config, { ws: { intents: intents } });
+
+// Initialize client
+function init() {
+  client.loadEvents('./src/events');
+  client.loadCommands('./src/commands');
+  client.loadTopics('./data/trivia');
+  client.login(client.token);
+}
+
+init();
+
+process.on('unhandledRejection', err => client.logger.error(err));
+
+    const http = require('http');
+    const server = http.createServer((req, res) => {
+      res.writeHead(200);
+      res.end('Bot Online');
+    });
+    server.listen(3000);
